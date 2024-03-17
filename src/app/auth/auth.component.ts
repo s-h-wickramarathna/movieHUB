5d6f58a30bd997434c7590b3436011f36680b6af
unknown <sanchithaheashan655@gmail.com>
2024-03-17 17:56:37 +0530

    reactive form validation bug fixed

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,59 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../component/footer/footer.component';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [
+    FooterComponent,
+    CommonModule,
+    ReactiveFormsModule, 
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
+
 export class AuthComponent {
+  signUpForm: FormGroup = new FormGroup({
+    signUpFirstname: new FormControl(''),
+    signUpLastname: new FormControl(''),
+  });
 
-constructor(private routes: Router){
+  signUpFormSibmitted = false;
+  constructor(private routes: Router, private formbuider: FormBuilder) {}
 
-}
+  ngOnInit(): void {
+    this.signUpForm = this.formbuider.group({
+      signUpFirstname: ['', Validators.required],
+      signUpLastname: ['', Validators.required],
+      signUpGender: ['', Validators.required],
+      signUpPhoneNumber:['',[Validators.required,Validators.pattern('07[2467][0-9]{7}')]],
+      signUpEmailAddress:['',[Validators.required,Validators.email]],
+    });
+  }
 
-login(){
-  this.routes.navigate(['/home']);
-}
+  get signupFormValidate(){
+    return this.signUpForm.controls;
+  }
+
+  onSubmit(): void {
+    this.signUpFormSibmitted = true;
+
+    if (this.signUpForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.signUpForm.value, null, 2));
+  }
+
+
+
+  login(): void {
+    this.routes.navigate(['/home']);
+  }
 }
