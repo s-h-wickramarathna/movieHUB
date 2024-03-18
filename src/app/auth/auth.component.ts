df2cae7a1176771fce4ee35e779d77d0903b2366
unknown <sanchithaheashan655@gmail.com>
2024-03-18 16:34:23 +0530

    signin form validation done

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../component/footer/footer.component';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
-import PasswordMatch from '../utils/PasswordMatch';
+import PasswordMatch from '../utils/passwordMatch';
 
 @Component({
   selector: 'app-auth',
@@ -13,42 +13,61 @@ import PasswordMatch from '../utils/PasswordMatch';
   imports: [
     FooterComponent,
     CommonModule,
-    ReactiveFormsModule, 
+    ReactiveFormsModule,
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+
   signUpForm: FormGroup = new FormGroup({
     signUpFirstname: new FormControl(''),
     signUpLastname: new FormControl(''),
+    signUpGender: new FormControl(''),
+    signUpPhoneNumber: new FormControl(''),
+    signUpEmailAddress: new FormControl(''),
+    signUpPassword: new FormControl(''),
+    signUpConfirmPassword: new FormControl(''),
+    acceptTerms: new FormControl(''),
+  });
+
+  loginForm: FormGroup = new FormGroup({
+    signInEmail: new FormControl(''),
+    signInPassword: new FormControl(''),
   });
 
   signUpFormSibmitted = false;
-  constructor(private routes: Router, private formbuider: FormBuilder) {}
+  loginFormSibmitted = false;
+  constructor(private routes: Router, private formbuider: FormBuilder) { }
 
   ngOnInit(): void {
+    
     this.signUpForm = this.formbuider.group({
       signUpFirstname: ['', Validators.required],
       signUpLastname: ['', Validators.required],
       signUpGender: ['', Validators.required],
-      signUpPhoneNumber:['',[Validators.required,Validators.pattern('07[2467][0-9]{7}')]],
-      signUpEmailAddress:['',[Validators.required,Validators.email]],
-      signUpPassword:['',Validators.required],
-      signUpConfirmPassword:['',Validators.required],
-      acceptTerms:['', Validators.requiredTrue]
-    },{
+      signUpPhoneNumber: ['', [Validators.required, Validators.pattern('07[2467][0-9]{7}')]],
+      signUpEmailAddress: ['', [Validators.required, Validators.email]],
+      signUpPassword: ['', Validators.required],
+      signUpConfirmPassword: ['', Validators.required],
+      acceptTerms: ['', Validators.requiredTrue]
+    }, {
       validators: [PasswordMatch.match('signUpPassword', 'signUpConfirmPassword')],
+    });
+
+    this.loginForm = this.formbuider.group({
+      signInEmail: ['', [Validators.required,Validators.email]],
+      signInPassword: ['', Validators.required],
     });
   }
 
 
-  get signupFormValidate(){
+  get signupFormValidate() {
     return this.signUpForm.controls;
   }
 
-  onSubmit(): void {
+  onSignUp(): void {
     this.signUpFormSibmitted = true;
 
     if (this.signUpForm.invalid) {
@@ -58,6 +77,19 @@ export class AuthComponent {
     console.log(JSON.stringify(this.signUpForm.value, null, 2));
   }
 
+  get loginFormValidate(){
+    return this.loginForm.controls;
+  }
+
+  onLogin(): void{
+    this.loginFormSibmitted = true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    console.log(JSON.stringify(this.loginForm.value));
+  }
 
 
   login(): void {
