11752374e634960e85fe637ee18fdc0fcdaf361a
unknown <sanchithaheashan655@gmail.com>
2024-03-23 10:01:58 +0530

    film watch page breadcrumb added

--- src/app/watch-film/watch-film.component.ts
+++ src/app/watch-film/watch-film.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../component/footer/footer.component';
+import { NavBarComponent } from '../component/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-watch-film',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FooterComponent,
+    NavBarComponent,
+  ],
   templateUrl: './watch-film.component.html',
   styleUrl: './watch-film.component.css'
 })
