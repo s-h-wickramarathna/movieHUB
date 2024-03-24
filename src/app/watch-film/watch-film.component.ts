79186360a0318c81b7d4080c3d4502651b257ab4
unknown <sanchithaheashan655@gmail.com>
2024-03-24 21:23:11 +0530

    navbar style chaneged

--- src/app/watch-film/watch-film.component.ts
+++ src/app/watch-film/watch-film.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FooterComponent } from '../component/footer/footer.component';
 import { NavBarComponent } from '../component/nav-bar/nav-bar.component';
+import { CardsComponent } from '../component/cards/cards.component';
 
 @Component({
   selector: 'app-watch-film',
@@ -10,6 +11,7 @@ import { NavBarComponent } from '../component/nav-bar/nav-bar.component';
     CommonModule,
     FooterComponent,
     NavBarComponent,
+    CardsComponent,
   ],
   templateUrl: './watch-film.component.html',
   styleUrl: './watch-film.component.css'
