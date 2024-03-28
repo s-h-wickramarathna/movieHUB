946da5e1ef71a18fbc68f9b5f0bb009eb9911d9a
unknown <sanchithaheashan655@gmail.com>
2024-03-28 18:17:50 +0530

    allfilms page slide bar structure created

--- src/app/all-films/all-films.component.ts
+++ src/app/all-films/all-films.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { NavBarComponent } from '../component/nav-bar/nav-bar.component';
 import { CardsComponent } from '../component/cards/cards.component';
 
@@ -10,11 +10,50 @@ import { CardsComponent } from '../component/cards/cards.component';
     CommonModule,
     NavBarComponent,
     CardsComponent,
-  
+
   ],
   templateUrl: './all-films.component.html',
   styleUrl: './all-films.component.css'
 })
 export class AllFilmsComponent {
+  constructor(private elementRef: ElementRef) { }
+
+  sideMenuHidden() {
+    // Catching element by ID
+    const sideBar: HTMLElement = this.elementRef.nativeElement.querySelector('#SidBarMain');
+    const menuIconBlock: HTMLElement = this.elementRef.nativeElement.querySelector('#menuIconBlock');
+    const menuIconHidden: HTMLElement = this.elementRef.nativeElement.querySelector('#menuIconHidden');
+
+    // Modifying CSS styles
+    if (sideBar) {
+      sideBar.classList.remove('left-0');
+      menuIconBlock.classList.remove('hidden');
+      menuIconHidden.classList.remove('block');
+      sideBar.classList.add('-left-full');
+      menuIconBlock.classList.add('block');
+      menuIconHidden.classList.add('hidden');
+  
+
+    }
+  }
+
+  sideMenuBlock() {
+    const sideBar: HTMLElement = this.elementRef.nativeElement.querySelector('#SidBarMain');
+    const menuIconBlock: HTMLElement = this.elementRef.nativeElement.querySelector('#menuIconBlock');
+    const menuIconHidden: HTMLElement = this.elementRef.nativeElement.querySelector('#menuIconHidden');
+
+    // Modifying CSS styles
+    if (sideBar) {
+      sideBar.classList.remove('-left-full');
+      menuIconBlock.classList.remove('block');
+      menuIconHidden.classList.remove('hidden');
+      sideBar.classList.add('left-0');
+      menuIconBlock.classList.add('hidden');
+      menuIconHidden.classList.add('block');
+
+
+    }
+  }
+
 
 }
